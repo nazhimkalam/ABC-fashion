a16ca72a4792cdd4b6df7fb9edfeff099c706028
Nazhim Kalam <nazhimkalam@gmail.com>
2024-09-21 15:24:26 +1000

    creating a new build

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5001;
 const API_URL = process.env.API_URL || "";
 const auth = {
     username: process.env.API_USERNAME || "",
-    password: process.env.API_PASSWORD || ""
+    password: process.env.API_PASSWORD || "",
 };
 app.use((0, cors_1.default)({
     origin: "*",
@@ -34,34 +34,70 @@ app.use((0, cors_1.default)({
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 // Function to check if a shipment is delayed based on sample data
-function isDelayed(shipment) {
+const isDelayed = (shipment) => {
     const startDate = new Date(shipment.start);
     const endDate = shipment.end ? new Date(shipment.end) : new Date();
     const diffDays = Math.ceil((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));
     // Check if the shipment status is not delivered or if it took more than 7 days
     return shipment.status !== "delivered";
-}
-// Function to fetch delayed shipments
-function fetchDelayedShipments() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const response = yield axios_1.default.get(API_URL, { auth });
-            const shipments = response.data.data;
-            const delayedShipments = shipments.filter(isDelayed);
-            return delayedShipments;
-        }
-        catch (error) {
-            console.error("Error fetching shipments:", error);
-            return [];
+};
+const getDashboardStats = (shipmentList) => __awaiter(void 0, void 0, void 0, function* () {
+    let totalInTransit = 0;
+    let totalFailedDelivery = 0;
+    let totalOutForDelivery = 0;
+    shipmentList.forEach((shipment) => {
+        switch (shipment.status) {
+            case "in_transit":
+                totalInTransit++;
+                break;
+            case "failed_delivery":
+                totalFailedDelivery++;
+                break;
+            case "out_for_delivery":
+                totalOutForDelivery++;
+                break;
+            default:
+                break;
         }
     });
-}
+    return {
+        totalInTransit,
+        totalFailedDelivery,
+        totalOutForDelivery,
+    };
+});
+// Function to fetch delayed shipments
+const fetchDelayedShipments = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield axios_1.default.get(API_URL, { auth });
+        const shipments = response.data.data;
+        const delayedShipments = shipments.filter(isDelayed);
+        const dashboardStats = yield getDashboardStats(delayedShipments);
+        return { delayedShipments, dashboardStats };
+    }
+    catch (error) {
+        console.error("Error fetching shipments:", error);
+        return undefined;
+    }
+});
 // Schedule a cron job to run every minute
+// Cron Job Schedule: * * * * *
+// *     *     *     *     *
+// |     |     |     |     |
+// |     |     |     |     |
+// |     |     |     |     +--- Day of the Week (0 - 6) (0 is Sunday, 6 is Saturday)
+// |     |     |     +--------- Month (1 - 12)
+// |     |     +--------------- Day of the Month (1 - 31)
+// |     +--------------------- Hour (0 - 23)
+// +--------------------------- Minute (0 - 59)
+//
+// In this case: "* * * * *" means the job will run every minute of every hour,
+// of every day, of every month, and every day of the week.
 const job = new cron_1.CronJob("* * * * *", () => {
     console.log("Running cron job to check for delayed shipments...");
-    fetchDelayedShipments().then((delayedShipments) => {
-        if (delayedShipments && delayedShipments.length > 0) {
-            console.log("Delayed Shipments:", delayedShipments);
+    fetchDelayedShipments().then((response) => {
+        if (response && response.delayedShipments.length > 0) {
+            console.log("Delayed Shipments:", response.delayedShipments);
             // Here, send email or SMS alerts to notify customers or admin
             // e.g., using Twilio, SendGrid, or another notification service
             // here we can decide on giving a discount,
@@ -71,7 +107,6 @@ const job = new cron_1.CronJob("* * * * *", () => {
             console.log("No delayed shipments at this time.");
         }
     });
-    ;
 });
 job.start();
 // Function to generate a text file of delayed shipments
@@ -93,8 +128,8 @@ const generateDelayedShipmentsFile = (delayedShipments) => {
 // API endpoint to manually trigger shipment fetching (for testing purposes)
 app.get("/delayed-shipments", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const delayedShipments = yield fetchDelayedShipments();
-        res.json({ delayedShipments });
+        const response = yield fetchDelayedShipments();
+        res.json(response);
     }
     catch (error) {
         console.error("Error fetching shipments:", error);
@@ -104,7 +139,7 @@ app.get("/delayed-shipments", (req, res) => __awaiter(void 0, void 0, void 0, fu
 // API endpoint to download delayed shipments as a text file
 app.get("/download-delayed-shipments", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const delayedShipments = yield fetchDelayedShipments();
+        const delayedShipments = yield fetchDelayedShipments().then((response) => response === null || response === void 0 ? void 0 : response.delayedShipments);
         if (delayedShipments && delayedShipments.length > 0) {
             const filePath = generateDelayedShipmentsFile(delayedShipments);
             res.download(filePath, "delayed_shipments.txt", (err) => {
